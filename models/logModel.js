0fcb406aa6f8f1d65cfccba74220735002338531
Jorge Jared Torres Placencia <2022030143@upsin.edu.mx>
2025-05-22 02:21:24 -0700

    Se arregló zona horaria en el registro de actividades

--- models/logModel.js
+++ models/logModel.js
@@ -2,8 +2,13 @@ const db = require('../db');
 
 const logModel = {
   registrar: (usuario_id, accion, descripcion, callback) => {
-    const sql = 'INSERT INTO log_actividad (usuario_id, accion, descripcion, fecha) VALUES (?, ?, ?, NOW())';
-    db.query(sql, [usuario_id, accion, descripcion], callback);
+    const now = new Date();
+    const offsetMazatlan = 7 * 60 * 60 * 1000; 
+    const localTime = new Date(now.getTime() - offsetMazatlan);
+    const formattedTime = localTime.toISOString().slice(0, 19).replace('T', ' ');
+    
+    const sql = 'INSERT INTO log_actividad (usuario_id, accion, descripcion, fecha) VALUES (?, ?, ?, ?)';
+    db.query(sql, [usuario_id, accion, descripcion, formattedTime], callback);
   },
 
   obtenerLogs: (callback) => {
